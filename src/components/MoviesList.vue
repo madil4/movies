668ecd216b3df44469d7e5bc07f1f61778b36607
Mohamed Adil <me@mohamedadil.com>
2022-11-26 17:27:45 +0100

    feat: add a MovieList component (#7)
    close #6

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import MovieItem from "./MovieItem.vue";
+
+export default {
+  components: { MovieItem },
+  data() {
+    return {
+      movies: [
+        { id: 1, title: "Spiderman" },
+        { id: 2, title: "Batman" },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <ol>
+    <MovieItem v-for="item in movies" :movie="item" :key="item.id"> </MovieItem>
+  </ol>
+</template>
