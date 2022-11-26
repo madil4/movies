36bb1d0172c3911345a776787198dff02077afcc
Mohamed Adil <me@mohamedadil.com>
2022-11-26 16:42:38 +0100

    feat: add a movie component (#5)
    * clean up startup components 
    
    close #3

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      id: 1,
+      title: "Spider man",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="wrapper">
+    <span>{{ title }}</span>
+    <button>delete</button>
+    <button>edit</button>
+  </div>
+</template>
+
+<style>
+.wrapper {
+  border: 1pt solid burlywood;
+  padding: 5pt 5pt;
+}
+
+button {
+  float: right;
+  margin-left: 5pt;
+}
+</style>
