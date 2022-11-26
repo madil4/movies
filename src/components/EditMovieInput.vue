ef7706b91c381fd3fe7b89d093e18afb266aac03
Mohamed Adil <me@mohamedadil.com>
2022-11-26 23:26:58 +0100

    feat: edit a movie (#13)
    fix #12

---
+++ src/components/EditMovieInput.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { store } from "../store";
+export default {
+  props: ["movie"],
+  data() {
+    return {
+      text: this.movie.title,
+      store,
+    };
+  },
+  methods: {
+    edit() {
+      if (this.text) {
+        store.edit(this.movie.id, this.text);
+        store.editModeID = -1;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <input v-model="text" @keyup.enter="edit" />
+</template>
