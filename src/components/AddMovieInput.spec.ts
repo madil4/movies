44ee5166929c61d9ab71054505fce828fe88ea33
Mohamed Adil <me@mohamedadil.com>
2022-11-27 21:58:12 +0100

    fix: add tests for AddMovieInput
    close #20

---
+++ src/components/AddMovieInput.spec.ts
@@ -0,0 +1,54 @@
+import { describe, it, expect, vi, afterEach } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import AddMovieInput from "./AddMovieInput.vue";
+import { addMovie } from "@/firebase";
+
+vi.mock("@/firebase", () => {
+  return {
+    addMovie: vi.fn(),
+  };
+});
+
+describe("AddMovieInput", () => {
+  const wrapper = mount(AddMovieInput);
+  const input = wrapper.find("input");
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should default with an empty value", () => {
+    expect(input.element.value).toBe("");
+  });
+
+  it("should has a placeholder", () => {
+    expect(input.element.placeholder).toBe("Add new movie");
+  });
+
+  it("should not add a movie when the value is empty", async () => {
+    input.setValue("");
+    await input.trigger("keyup.enter");
+
+    expect(addMovie).not.toBeCalled();
+  });
+
+  it("should add a movie after pressing enter key", async () => {
+    const movie = "Spiderman";
+
+    input.setValue(movie);
+    await input.trigger("keyup.enter");
+
+    expect(addMovie).toHaveBeenCalledWith(movie);
+  });
+
+  it("should reset input value after adding a movie", async () => {
+    const movie = "Spiderman";
+
+    input.setValue(movie);
+    await input.trigger("keyup.enter");
+
+    expect(addMovie).toBeCalledWith(movie);
+    expect(input.element.value).toBe("");
+  });
+});
